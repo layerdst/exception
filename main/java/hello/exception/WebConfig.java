97f2b3e24382e103bc05176ce9c430d4a829a6e7
layerdst <bccfcc@hanmail.net>
2022-10-25 21:56:13 +0900

    interceptor

--- main/java/hello/exception/WebConfig.java
+++ main/java/hello/exception/WebConfig.java
@@ -1,9 +1,11 @@
 package hello.exception;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
@@ -22,5 +24,13 @@ public class WebConfig implements WebMvcConfigurer {
         filterFilterRegistration.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
 
         return filterFilterRegistration;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*/ico", "/error", "/error-page/**");
     }
 }
